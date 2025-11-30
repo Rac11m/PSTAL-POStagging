dd88d72ed4f65cb7f57a92bb81489bc032023070
Racim Saal <racimsaal@gmail.com>
2025-11-30 14:27:26 +0100

    add evaluation code

--- prediction_postag.py
+++ prediction_postag.py
@@ -6,6 +6,7 @@ from use_conllulib import CoNLLUReader, Util
 from torch.utils.data import TensorDataset, DataLoader
 from train_postag import read_corpus
 from conllu.serializer import serialize
+from conllu.models import TokenList
 
 
 if __name__ == "__main__" :  
@@ -22,10 +23,22 @@ if __name__ == "__main__" :
   
   words, _, _, _ = read_corpus(filename="../pstal-etu/sequoia/sequoia-ud.parseme.frsemcor.simple.dev", wordvocab=wordvocab, tagvocab=tagvocab, max_len=40, batch_size=32, train_mode=False, batch_mode=False)
   revtagvocab = Util.rev_vocab(tagvocab)
+  revwordvocab = Util.rev_vocab(wordvocab)
+
+  revwords = []
+  for sent in words:
+    revwords.append([revwordvocab[s] for s in sent])
+
+  sentences = []
 
   for sent in words:
     logits = model(torch.LongTensor([sent]))[0] # y_hat
-    print([revtagvocab[l.argmax()] for l in logits]) # t_hat
-    # print(sentence.serialize())
-
-
+    forms = [revwordvocab[w] for w in sent] 
+    upos = [revtagvocab[l.argmax()] for l in logits]
+    
+    conllu_format = [{"id": index, "form": w, "upos": t} for index, (w, t) in enumerate(zip(forms, upos), start=1)]
+    sentences.append(TokenList(conllu_format)) 
+  
+  with open('sequoia-ud.parseme.frsemcor.simple.pred', 'w', encoding="utf-8") as f:
+    f.writelines([sentence.serialize() + "\n" for sentence in sentences])
+  
